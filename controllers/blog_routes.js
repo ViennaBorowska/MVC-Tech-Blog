74868c11651e077e3e8a7173d74dc18cbbfaecc3
Vienna Borowska <vienna.borowska@hotmail.com>
2022-02-08 21:38:37 +0000

    Built up API routes in blog_routes.js

--- controllers/blog_routes.js
+++ controllers/blog_routes.js
@@ -1,18 +1,54 @@
 const router = require('express').Router();
+const Blogs = require('../models');
 
 //GET all blogs
-router.get('/', async (req, res) => {});
+router.get('/', async (req, res) => {
+  try {
+    const allBlogs = await Blogs.findAll();
+    res.json(allBlogs);
+  } catch (err) {
+    res.sendStatus(500).send(err);
+  }
+});
 
 //GET single blog
-router.get('/:id', async (req, res) => {});
+router.get('/:id', async (req, res) => {
+  try {
+    const singleBlog = await Blogs.findByPk(req.params.id);
+    res.json(singleBlog);
+  } catch (err) {
+    res.sendStatus(500).send(err);
+  }
+});
 
 //POST add new blog post
-router.post('/', async (req, res) => {});
+router.post('/', async (req, res) => {
+  try {
+    const newBlogpost = await Blogs.create(req.body);
+    res.json(newBlogpost);
+  } catch (err) {
+    res.sendStatus(500).send(err);
+  }
+});
 
 //PUT update a blog post
-router.put('/:id', async (req, res) => {});
+router.put('/:id', async (req, res) => {
+  try {
+    const updateBlogpost = await Blogs.update(req.body);
+    res.json(updateBlogpost);
+  } catch (err) {
+    res.sendStatus(500).send(err);
+  }
+});
 
 //DELETE a blog post
-router.delete('/:id', async (req, res) => {});
+router.delete('/:id', async (req, res) => {
+  try {
+    const deleteBlog = await Blogs.destroy(req.body);
+    res.json(deleteBlog);
+  } catch (err) {
+    res.sendStatus(500).send(err);
+  }
+});
 
 module.exports = router;
---
+++ models/index.js
@@ -0,0 +1,3 @@
+const Blogpost = require('./Blogpost');
+
+module.exports = { Blogpost };
