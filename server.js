863b37a3a646a902c1b17ee264c6d876dec6a563
Vienna Borowska <vienna.borowska@hotmail.com>
2022-02-08 19:08:28 +0000

    building up server.js file with more dependencies & functionality e.g handlebars/express-session & sequelize

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
+const path = require('path');
 const express = require('express');
 const session = require('express-session');
-//const routes = require('./controllers');
+const exphbs = require('express-handlebars');
 
+//const routes = require('./controllers');
 const sequelize = require('./config/connection');
+const helpers = require('./utils/helpers');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
@@ -20,8 +23,14 @@ const sess = {
 
 app.use(session(sess));
 
+const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 //app.use(routes);
 
